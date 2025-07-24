f3835e7b52029d66757ba19d07724336c315c400
angrezichatterbox <gouthammohanraj@gmail.com>
2025-07-24 10:25:57 +0530

    Uncomment verifyGeneratedCode function
    Signed-off-by: angrezichatterbox <gouthammohanraj@gmail.com>

--- codegen/src/test/java/org/web3j/codegen/GeneraterTestUtils.java
+++ codegen/src/test/java/org/web3j/codegen/GeneraterTestUtils.java
@@ -20,18 +20,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GeneraterTestUtils {
 
-//    public static void verifyGeneratedCode(String sourceFile) throws IOException {
-//        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-//        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
-//
-//        try (StandardJavaFileManager fileManager =
-//                compiler.getStandardFileManager(diagnostics, null, null)) {
-//            Iterable<? extends JavaFileObject> compilationUnits =
-//                    fileManager.getJavaFileObjectsFromStrings(
-//                            Collections.singletonList(sourceFile));
-//            JavaCompiler.CompilationTask task =
-//                    compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
-//            assertTrue(task.call(), "Generated contract contains compile time error");
-//        }
-//    }
+    public static void verifyGeneratedCode(String sourceFile) throws IOException {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+
+        try (StandardJavaFileManager fileManager =
+                compiler.getStandardFileManager(diagnostics, null, null)) {
+            Iterable<? extends JavaFileObject> compilationUnits =
+                    fileManager.getJavaFileObjectsFromStrings(
+                            Collections.singletonList(sourceFile));
+            JavaCompiler.CompilationTask task =
+                    compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
+            assertTrue(task.call(), "Generated contract contains compile time error");
+        }
+    }
 }
